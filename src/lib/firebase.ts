47d67fe99078aa7bf163be3a528ee52a8405f796
Younes Marzouk <younesmarzoukpro@gmail.com>
2025-11-24 18:01:46 +0000

    ضفت مشروع في استظافة و عمل بدون اظافة متغيرات بيئية اعتقد انه مشروع هناك

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmt0xTpNe4lLbPcGxRQ4HuBWZxgIve3r0",
-  authDomain: "khidmanow-e6c63.firebaseapp.com",
-  projectId: "khidmanow-e6c63",
-  storageBucket: "khidmanow-e6c63.firebasestorage.app",
-  messagingSenderId: "771104615194",
-  appId: "1:771104615194:web:6fe79bf12566da60d95aa3",
-  measurementId: "G-VTP3YS7VVH"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
